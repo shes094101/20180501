dc981aac783027ef75291e10dd978464381f00f1
shes094101 <32055777+shes094101@users.noreply.github.com>
2018-05-01 17:20:13 +0800

    work

--- 2/MainFrame.java
+++ 2/MainFrame.java
@@ -4,8 +4,8 @@ import java.awt.event.ActionListener;
 
 public class MainFrame extends JFrame {
 
-    private JButton jbtnAdd = new JButton("ADD");// 設置按鈕add
-    private JButton jbtnReduce = new JButton("Reduce");// 設置按鈕reduce
+    private JButton jbtnAdd = new JButton("-->");// 設置按鈕add
+    private JButton jbtnReduce = new JButton("<--");// 設置按鈕reduce
     private JButton jbtnexit = new JButton("Exit");// 設置按鈕exit
     private JLabel jlb =new JLabel("ლ(・ω・ლ)" );
     private int x = 0;//設置參數為0
@@ -20,11 +20,11 @@ public class MainFrame extends JFrame {
         this.setLocation(100, 50);            //設置位置
         this.setSize(800, 600);     //設置大小
 
-        jbtnAdd.setLocation(50, 60);              //將按鈕設定位置
+        jbtnAdd.setLocation(170, 60);              //將按鈕設定位置
         jbtnAdd.setSize(120, 30);  //設定按鈕大小
         this.add(jbtnAdd);                       //將按鈕設置在介面上
 
-        jbtnReduce.setLocation(170, 60);
+        jbtnReduce.setLocation(50, 60);
         jbtnReduce.setSize(120, 30);
         this.add(jbtnReduce);
 
